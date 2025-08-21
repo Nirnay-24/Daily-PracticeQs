5e7b4e57df2750f4b8df40176bc41b4f87c42ff8
Nirnay Arekar <164948569+Nirnay-24@users.noreply.github.com>
2025-08-21 09:01:48 +0530

    Day 1

---
+++ _0001_HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+/*
+ * Program: Hello World
+ * --------------------
+ * This is the basic Java program that prints "Hello World" to the console.
+ */
+
+public class _0001_HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World");  // Print Hello World
+    }
+}
