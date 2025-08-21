9e256800ea40bf897e34f020b25dfcc8882abb36
Nirnay Arekar <164948569+Nirnay-24@users.noreply.github.com>
2025-08-21 09:02:11 +0530

    Day 1

---
+++ _0002_SumOfNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/*
+ * Program: Sum of Two Numbers
+ * ---------------------------
+ * This program calculates the sum of two integers and prints the result.
+ */
+
+public class _0002_SumOfNumber {
+    public static void main(String args[]) {
+        int a = 6;  // Assign value to variable a
+        int b = 9;  // Assign value to variable b
+
+        int sum = a + b;  // Add values of a and b
+
+        System.out.println(sum);  // Print the sum
+    }
+}
